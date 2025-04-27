bc69489fe4fbf77f1dce44ac09f7273390c0c236
kh9sd <kevin9sd@gmail.com>
2025-04-27 19:09:00 -0400

    Add create note table script

--- create_note_table.py
+++ create_note_table.py
@@ -1,18 +1,18 @@
 from db_connect import load_config, connect
 
 def create_tables(conn):
-    print(f"{conn=}")
+    # print(f"{conn=}")
     with conn:
         with conn.cursor() as cur:
-            print(f"{cur=}")
+            #print(f"{cur=}")
             # execute the CREATE TABLE statement
-            cur.execute(open("create_tag_table.sql", "r").read())
+            cur.execute(open("create_note_table.sql", "r").read())
         # conn.commit()
-    print("Done creating table")
+    print("Done creating NOTE table")
 
 if __name__ == '__main__':
     config = load_config()
-    print(config)
+    # print(config)
     conn = connect(config)
 
     create_tables(conn)
