22ab0dbd0d137bb209b87a4c3f16a19805a0ba2b
kh9sd <kevin9sd@gmail.com>
2025-04-27 20:02:37 -0400

    Improve safe copy

--- safe_copy.py
+++ safe_copy.py
@@ -6,7 +6,9 @@ def safe_copy(src_path, dest_path):
     dest_fd = os.open(dest_path, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
 
     with os.fdopen(dest_fd, "w") as dest_f:
-        # TODO: copy rest of file metadata?
         with open(src_path) as src_f:
-            return shutil.copyfileobj(src_f, dest_f)
+            shutil.copyfileobj(src_f, dest_f)
 
+    # TODO: copy rest of file metadata?
+    # if we use Git to store these files, pointless, doesn't track
+    # shutil.copystat(src_path, dest_path)
