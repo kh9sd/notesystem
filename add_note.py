f69106d7690f129a6b459d186ba3ab0f3e0e2653
kh9sd <kevin9sd@gmail.com>
2025-04-28 15:58:44 -0400

    Take in filename as pos arg

--- add_note.py
+++ add_note.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from safe_copy import safe_copy
 import os
 
+import argparse
+
 def add_note(conn, rel_note_path):
     sql = """INSERT INTO notes(path)
              VALUES(%s) RETURNING id;"""
@@ -21,6 +23,15 @@ def add_note(conn, rel_note_path):
             print(f"Done adding note '{rel_note_path}' with id={returned_rows[0]}!")
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog="add_note",
+        description="Takes in absolute path to note, will save it to filesystem and DB."
+    )
+
+    parser.add_argument('abs_path_to_note')
+    args = parser.parse_args()
+    # print(args)
+    
     config = load_config()
     # print(config)
     conn = connect(config)
@@ -28,12 +39,8 @@ if __name__ == '__main__':
     root_dir_path = get_root_dir_path()
     # print(f"{root_dir_path=}")
 
-    # test file
-    shit = list(Path().glob("TEMP_FILE3.txt"))
-    assert(len(shit) == 1)
-
-    # TODO: how do I get this
-    new_file_abs_path = shit[0].resolve()
+    new_file_abs_path = Path(args.abs_path_to_note)
+    assert(new_file_abs_path.is_absolute())
     new_file_raw_name = new_file_abs_path.name
     # print(f"{new_file_raw_name=}")
 
