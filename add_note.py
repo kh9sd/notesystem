22195fc4869bbb05e1c9d4bf9b97fd8572b942f7
kh9sd <kevin9sd@gmail.com>
2025-04-28 15:13:42 -0400

    Initial note path saving to DB implemented

--- add_note.py
+++ add_note.py
@@ -3,12 +3,12 @@ from pathlib import Path
 from safe_copy import safe_copy
 import os
 
-def add_tag(conn, tag_name):
-    sql = """INSERT INTO tags(name)
+def add_note(conn, rel_note_path):
+    sql = """INSERT INTO notes(path)
              VALUES(%s) RETURNING id;"""
     with conn:
         with conn.cursor() as cur:
-            cur.execute(sql, (tag_name,))
+            cur.execute(sql, (rel_note_path,))
 
             # Contains result of INSERT from RETURNING clause
             # Should be exactly 1 element
@@ -16,7 +16,7 @@ def add_tag(conn, tag_name):
 
             assert(len(returned_rows) == 1)
 
-            print(f"Done adding tag '{tag_name}' with id={returned_rows[0]}!")
+            print(f"Done adding note '{rel_note_path}' with id={returned_rows[0]}!")
 
 if __name__ == '__main__':
     config = load_config()
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     # print(f"{root_dir_path=}")
 
     # test file
-    shit = list(Path().glob("TEMP_FILE.txt"))
+    shit = list(Path().glob("TEMP_FILE2.txt"))
     assert(len(shit) == 1)
 
     # TODO: how do I get this
@@ -47,9 +47,17 @@ if __name__ == '__main__':
     # We will be saving this relative path in the DB
     relative_saved_dir_from_root = Path('saved')
     relative_saved_file_path_from_root = relative_saved_dir_from_root / new_file_raw_name
-    print(f"{relative_saved_file_path_from_root=}")
+    #print(f"{relative_saved_file_path_from_root=}")
 
     new_saved_file_path = root_dir_path / relative_saved_file_path_from_root
 
     os.makedirs(root_dir_path / relative_saved_dir_from_root, exist_ok=True)
     safe_copy(new_file_abs_path, new_saved_file_path)
+
+    # TODO: possibility of desync, file save works, but DB save doesn't
+
+    # this is rather critical, we want slashs not backslash
+    relative_saved_file_path_str = relative_saved_file_path_from_root.as_posix()
+    #print(f"{relative_saved_file_path_str=}")
+
+    add_note(conn, relative_saved_file_path_str)
